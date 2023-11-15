b9e6b79084536ae991e71f014021653c25ed81a5
creationk <creationk@users.noreply.github.com>
2023-11-15 10:01:52 +0530

    Bug fixes

--- src/main/java/com/synchrony/dto/UserProfileDTO.java
+++ src/main/java/com/synchrony/dto/UserProfileDTO.java
@@ -1,6 +1,5 @@
 package com.synchrony.dto;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.synchrony.entity.Image;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,7 +15,6 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserProfileDTO {
-    private final ObjectMapper objectMapper = new ObjectMapper();
     private String firstName;
     private String lastName;
     private List<Image> imageList;
